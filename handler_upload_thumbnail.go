3ed030ccadcb711d88c813dcd775736cca31130f
mentalcaries <neexis@gmail.com>
2025-09-01 06:57:57 -0400

    save file to disk

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +33,51 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	fileExt := strings.Split(contentType, "/")[1]
+
+	// fileData, err := io.ReadAll(file)
+
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err == sql.ErrNoRows {
+		respondWithError(w, http.StatusNotFound, "no video found", err)
+		return
+	}
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Auhorization required", nil)
+		return
+	}
+
+	fileName := fmt.Sprintf("%v.%v", videoID, fileExt)
+	path := filepath.Join(cfg.assetsRoot, fileName)
+	newImageFile, err := os.Create(path)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create file", err)
+		return
+	}
+
+	_, err = io.Copy(newImageFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not write to file", err)
+	}
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:%v/assets/%v.%v", cfg.port, videoID, fileExt)
+	metadata.ThumbnailURL = &thumbnailUrl
+
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "could not update video", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, metadata)
 }
